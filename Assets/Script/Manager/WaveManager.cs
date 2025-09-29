b58b37505326f7a6adeca19feaa335870a48fca7
Henry Pham <113865174+PhatPham7103@users.noreply.github.com>
2025-09-29 00:46:12 -0500

    WaveManagerv1

--- Assets/Script/Manager/WaveManager.cs
+++ Assets/Script/Manager/WaveManager.cs
@@ -6,19 +6,71 @@ using NaughtyAttributes;
 
 public class WaveManager : MonoBehaviour
 {
-    [Header(" Settings ")]
-    [SerializeField] private Wave[] wave;
+    [Header(" Elements ")]
+    [SerializeField] private Player player;
 
+    [Header(" Settings ")]
+    [SerializeField] private float waveDuration;
+    private float timer;
+
+    [Header(" Waves ")]
+    [SerializeField] private Wave[] waves;
+    private List<float> localCounters = new List<float>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        localCounters.Add(1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer < waveDuration)
+        {
+            ManageCurrentWave();
+        }
+    }
+
+    private void ManageCurrentWave()
+    {
+        Wave currentWave = waves[0];
+
+        for (int i = 0; i < currentWave.segments.Count; i++)
+        {
+            WaveSegment segment = currentWave.segments[i];
+
+            float tStart = segment.tStartEnd.x / 100 * waveDuration;
+            float tEnd = segment.tStartEnd.y / 100 * waveDuration;
+
+            if (tStart > timer || timer > tEnd)
+            {
+                continue;
+            }
+            float timeSinceSegmentStart = timer - tStart;
+
+            float spawnDelay = 1f / segment.spawnFrequency;
+
+
+            if(timeSinceSegmentStart / spawnDelay > localCounters[i])
+            {
+                Instantiate(segment.prefab, GetSpawnPosition(), Quaternion.identity, transform);
+                localCounters[i]++;
+            }
+        }
+
+        timer += Time.deltaTime;
+    }
+
+    private Vector2 GetSpawnPosition()
+    {
+        Vector2 direction = Random.onUnitSphere;
+        Vector2 offset = direction.normalized * Random.Range(6, 10);
+        Vector2 targetPosition = (Vector2)player.transform.position + offset;
+
+        targetPosition.x = Mathf.Clamp(targetPosition.x, -18, 18);
+        targetPosition.y = Mathf.Clamp(targetPosition.y, -8, 8);
+
+        return targetPosition;
     }
 }
 
@@ -33,5 +85,7 @@ public struct Wave
 [System.Serializable]
 public struct WaveSegment
 {
+    [MinMaxSlider(0, 100)] public Vector2 tStartEnd;
+    public float spawnFrequency;
     public GameObject prefab;
 }
