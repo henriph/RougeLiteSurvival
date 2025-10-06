2b55ae73dce278619a8061eefc802b7e1e807ea2
Henry Pham <113865174+PhatPham7103@users.noreply.github.com>
2025-10-06 11:34:11 -0500

    Drops Implementation

--- Assets/Script/Drops/Candy.cs
+++ Assets/Script/Drops/Candy.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Candy : MonoBehaviour
 {
+    private bool collected;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,4 +17,36 @@ public class Candy : MonoBehaviour
     {
         
     }
+
+    public void Collect(Transform playerTransform)
+    {
+        if (collected)
+            return;
+
+        collected = true;
+
+        StartCoroutine(MoveTowardPlayer(playerTransform));
+    }
+
+   IEnumerator MoveTowardPlayer(Transform playerTransform)
+    {
+        float timer = 0;
+        Vector2 initialPoint = transform.position;
+        
+        while(timer < 1)
+        {
+            transform.position = Vector2.Lerp(initialPoint, playerTransform.position, timer);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+
+        Collected();
+    }
+
+    private void Collected()
+    {
+        gameObject.SetActive(false);
+    }
 }
+
--- Assets/Script/Player/PlayerDetection.cs
+++ Assets/Script/Player/PlayerDetection.cs
@@ -2,11 +2,23 @@ using UnityEngine;
 
 public class PlayerDetection : MonoBehaviour
 {
+    [Header(" Colliders ")]
+    [SerializeField] private CircleCollider2D playerCollider;
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if(collider.TryGetComponent(out Candy candy))
+        //Check if the object entering the trigger is the Candy component.
+        if (collider.TryGetComponent(out Candy candy))
         {
+            if (!collider.IsTouching(playerCollider))
+                return;
 
+            //The trigger event already confirms contact.
+            Debug.Log("Collect candy!");
+            candy.Collect(transform);
         }
+    }
+
+    private void CandyDetection()
+    {
     }
 }
